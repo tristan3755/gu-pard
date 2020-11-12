03a7f88d4cf02a4f8e79ca44dd572b7ef900a127
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-11-12 16:05:36 +0100

    guepards

--- javascript.js
+++ javascript.js
@@ -1,52 +1,33 @@
+document.querySelector(".contenu1").addEventListener("mouseover", (event) => {
+  hover();
+  nav();
+});
 
-document.querySelector(".contenu1").addEventListener("mouseover",(event)=>{
+document.querySelector(".contenu1").addEventListener("mouseout", (event) => {
+  dehover();
+});
 
-hover()
-nav()
+function hover() {
+  let back = document.querySelector(".contener1");
+  back.style.width = "100%";
+  back.style.transition = "5s";
 
-})
-
-document.querySelector(".contenu1").addEventListener("mouseout",(event)=>{
-
-   
-    dehover()
-    
-})
-
-
-function hover(){
-
-    let back=document.querySelector(".contener1")
-    back.style.width="100%"
-    back.style.transition="5s"
-
-    let back2=document.querySelector(".contenu1")
-    back2.style.width="100%"
-    back2.style.transition="5s"
-
-
+  let back2 = document.querySelector(".contenu1");
+  back2.style.width = "100%";
+  back2.style.transition = "5s";
 }
 
-function dehover(){
+function dehover() {
+  let back = document.querySelector(".contener1");
+  back.style.width = "45%";
+  back.style.transition = "5s";
 
-    let back=document.querySelector(".contener1")
-    back.style.width="45%"
-    back.style.transition="5s"
-
-    let back2=document.querySelector(".contenu1")
-    back2.style.width="45%"
-    back2.style.transition="5s"
-
-
+  let back2 = document.querySelector(".contenu1");
+  back2.style.width = "45%";
+  back2.style.transition = "5s";
 }
 
-function nav(){
-
-    let nav=document.querySelector("header")
-    nav.style.display="flex"
-
-    
-   
-
-
+function nav() {
+  let nav = document.querySelector("header");
+  nav.style.display = "flex";
 }
